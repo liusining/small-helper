444ed5ce64ad4567c7f58990f9b8ec61aada8782
sida <siningdream@gmail.com>
2018-02-20 01:40:25 +0800

    feature: add some guide for users

--- popup/util.js
+++ popup/util.js
@@ -9,15 +9,20 @@ let dataPromise = new Promise(function (resolve, reject) {
     chrome.tabs.query({
         url: 'https://big.one/accounts'
     }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            msg: "query_page"
-        }, function (resp) {
-            if (resp) {
-                resolve([resp.labels, resp.data]);
-            } else {
-                reject('BigONE Account Page Not Found');
-            }
-        });
+        if (tabs[0]) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                msg: "query_page"
+            }, function (resp) {
+                if (resp) {
+                    resolve([resp.labels, resp.data]);
+                } else {
+                    reject('require_reload');
+                }
+            });
+        } else {
+            reject('no_bigone_account');
+        }
+
     });
 });
 
@@ -26,7 +31,7 @@ dataPromise.then(function ([labels, data]) {
     let sum = data.reduce((accum, current) => accum + current);
 
     for (let i = 0; i < labels.length; i++) {
-      labels[i] += ` ${(data[i] / sum * 100).toFixed(1)}%`
+        labels[i] += ` ${(data[i] / sum * 100).toFixed(1)}%`
     }
 
     let colors = [];
@@ -51,4 +56,34 @@ dataPromise.then(function ([labels, data]) {
             responsive: true
         }
     });
-}, function (reason) {});
+}, function (reason) {
+    let body = document.querySelector('body'),
+        div = document.createElement('div'),
+        header = document.createElement('h3'),
+        note = document.createElement('div'),
+        ctx = document.getElementById('canvas');
+    div.setAttribute("style", "width: 300px; height: 200px;");
+    body.insertBefore(div, ctx);
+    body.removeChild(ctx);
+    div.insertBefore(header, null);
+    div.insertBefore(note, null);
+
+    if (reason === 'no_bigone_account') {
+        header.innerHTML = `Open a BigONE account<br><small class="chinese">请打开币一账户</small>`;
+        note.innerHTML = `<p>BigONE account page not found. Please open one.</p>
+          <p class="chinese">没有找到币一账户的网页，请打开一个。</p>`;
+    } else if (reason === 'require_reload') {
+        header.innerHTML = `Refresh BigONE account<br><small class="chinese">请刷新币一账户</small>`;
+        note.innerHTML = `<p>This extension lost connection to BigONE account. Please refresh the BigONE.</p>
+          <p class="chinese">插件无法连接到币一网页了，请刷新。</p>`;
+    } else {
+        header.innerHTML = `Error<br><small class="chinese">有错误</small>`;
+        note.innerHTML = `<p>${reason}</p>
+          <p class="report">You can report this error
+          <a href="https://github.com/liusining/small-helper/issues" id="github">on github</a>.</p>`;
+        document.getElementById('github').onclick = function (e) {
+            window.open(this.href);
+            e.preventDefault();
+        };
+    }
+});
